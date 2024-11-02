f2c9df31f2043d3a5d3d39be2cb2bf85e2fa974a
Egor763 <132680057+Egor763@users.noreply.github.com>
2024-11-02 16:14:31 +0400

    login вёрстка

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,26 @@
 import './Login.css';
 
 export function Login() {
-  return <section className='login'>jlkjklkj</section>;
+  return (
+    <section className='login'>
+      <h2 className='login__title'>Вход</h2>
+      <form className='login__form'>
+        <input
+          className='login__input'
+          id='input__email'
+          type='email'
+          placeholder='Email'
+        />
+        <input
+          className='login__input'
+          id='input__password'
+          type='password'
+          placeholder='Пароль'
+        />
+        <button className='login__button hover__link' type='button'>
+          Войти
+        </button>
+      </form>
+    </section>
+  );
 }
